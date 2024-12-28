ca3b74f1ec7deefa088023ccd3f44ae0a96dc9bb
fab-jobber <fabienne.c@getjobber.com>
2024-12-28 17:10:31 -0500

    Created dbt modeled schema

--- dbt_strava/models/modeled/running.sql
+++ dbt_strava/models/modeled/running.sql
@@ -1,15 +1,19 @@
 {{ config(
-    materialized='view')
+    materialized='incremental',
+    unique_key='id')
 }}
 
+-- Purpose of this model is to track running distances against goals
+-- 200 days of activity
+-- distance 650km (551km in 2024)
 SELECT
     id
+    , start_at_local
     , name
-    , distance/1000.0 AS distance
-    , moving_time/60.0 AS moving_time -- define as minutes
-    , total_elevation_gain AS total_elevation_gain_meters
+    , distance
+    , moving_time -- define as minutes
+    , total_elevation_gain_meters
     , sport_type
-    , start_date_local AS start_at_local
     , achievement_count
     , kudos_count
     , description -- future data collection, assess how to capture
@@ -29,4 +33,11 @@ SELECT
     , similar_activities__min_average_speed
     , similar_activities__mid_average_speed
     , similar_activities__max_average_speed
-FROM {{ source("strava", "full_load") }}
+    , CURRENT_LOCALTIME() AS load_at
+FROM {{ ref("stg_full_load") }}
+WHERE
+    sport_type = 'Run'
+
+    {% if is_incremental() %}
+    AND start_at_local >= (select coalesce(max(start_at_local),'2020-01-01') from {{ this }} )
+    {% endif %}
